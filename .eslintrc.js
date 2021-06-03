f13a93149623c80e579b3dd24cba98bad2206e2f
Luiz Henrique Dias <luiz.dias@farme.com.br>
2021-06-03 17:22:53 -0300

    add configurations

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb'
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 12,
+		sourceType: 'module'
+	},
+	plugins: [
+		'react'
+	],
+	rules: {
+		'react/jsx-indent': [2, 'tab'],
+		'react/jsx-indent-props': [2, 'tab'],
+		'react/jsx-filename-extension': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'react/prop-types': 'off',
+		'react/jsx-props-no-spreading': 'off',
+
+		'no-param-reassign': 'off',
+		'import/first': 'off',
+		'import/named': 'error',
+		'import/namespace': 'error',
+		'import/default': 'error',
+		'import/export': 'error',
+		'import/extensions': 'off',
+		'import/no-unresolved': 'off',
+		'import/no-extraneous-dependencies': 'off',
+		'import/prefer-default-export': 'off',
+		'prefer-promise-reject-errors': 'off',
+		'prefer-destructuring': 'off',
+		'no-async-promise-executor': 'off',
+		'comma-dangle': ['error', 'never'],
+		'no-unreachable': 'warn',
+		'max-len': 0,
+		'no-constant-condition': 0,
+		'no-tabs': 0,
+		'no-bitwise': 0,
+		indent: ['error', 'tab', { SwitchCase: 1, ignoredNodes: ['TemplateLiteral'] }],
+		'arrow-parens': ['error', 'as-needed'],
+		'func-names': 0,
+		'template-curly-spacing': 'off',
+		'nonblock-statement-body-position': ['error', 'below'],
+		curly: ['error', 'multi-or-nest'],
+		'no-unused-vars': 'warn',
+		'no-underscore-dangle': 0,
+		camelcase: 0,
+		'no-continue': 0,
+		'import/no-cycle': 0,
+		'consistent-return': 0,
+		'no-plusplus': 0
+	}
+};
